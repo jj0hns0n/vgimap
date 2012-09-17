74bbb95ce1fb1462da66c1c5a5356669024445c5
Jeffrey Johnson <jjohnson@opengeo.org>
2012-09-16 21:29:03 -0700

    Stub in OSMWFSAdapter

--- vgimap/services/wfs.py
+++ vgimap/services/wfs.py
@@ -18,7 +18,53 @@ def get_response(url):
         return e
 
 
+class OSMWFSAdapter(WFSAdapter):
+
+    def __init__(self):
+        pass
+
+    def get_feature_descriptions(self, request, **params):
+        namespace = request.build_absolute_uri().split('?')[0] + "/schema"
+        extent = (-180,-90,180,90)
+
+        return [FeatureDescription(
+                ns=namespace,
+                ns_name="osm",
+                name="OpenStreetMap",
+                abstract="OpenStreetMap Data",
+                title="OpenStreetMap Data",
+                keywords=['openstreetmap', 'osm', 'vgi'],
+                srs="EPSG:4326",
+                bbox=[-180,-90,180,90],
+                schema=namespace
+            )]
+
+    def list_stored_queries(self, request):
+        pass
+
+    def get_features(self, request, params):
+        # Can eventually support stored queries here
+        return self.AdHocQuery(request, params)
+
+    def AdHocQuery(self, request, params):
+        type_names = params.cleaned_data['type_names'] # only support one type-name at a time (model) for now
+        flt = params.cleaned_data['filter'] # filter should be in JSON 
+        bbox = params.cleaned_data['bbox'] 
+        sort_by = params.cleaned_data['sort_by']
+        count = params.cleaned_data['count']
+        if not count:
+            count = params.cleand_data['max_features'] 
+        start_index = params.cleaned_data['start_index']
+        srs_name = params.cleaned_data['srs_name'] # assume bbox is in this
+        srs_format = params.cleaned_data['srs_format'] # this can be proj, None (srid), srid, or wkt.
+        
+        if flt:
+            pass
+        else:
+            pass
+
 class TwitterWFSAdapter(WFSAdapter):
+
     def __init__(self):
         pass
 
